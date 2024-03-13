23e096c4aeab79653e308c6a34b280a440397b3c
Alex Hampton <ahampton5158@gmail.com>
2024-03-13 13:03:30 -0400

    Change data from list to table with headers

--- src/DataList.js
+++ src/DataList.js
@@ -2,10 +2,19 @@ import React from 'react'
 import './DataList.css'
 
 const DataList = ({currentData}) => {
+  const tableHeaders = Object.keys(currentData[0])
   return (
-    <ul className='DataList'>
-      {currentData.map(item => <li key={item.id}>{JSON.stringify(item)}</li>)}
-    </ul>
+    <table className='DataList'>
+      {tableHeaders.map(header => <th>{header}</th>)}
+      {currentData.map(item => (
+        <tr key={item.id}>
+          {Object.keys(item).map(key => (
+            <td>{JSON.stringify(item[key])}</td>
+          ))}
+        </tr>
+      ))}
+    </table>
+
   )
 }
 
